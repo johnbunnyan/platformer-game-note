6b0941110297c1e8c1e8cf56e186b7a3b4993853
johnbunnyan <jsk717543@gmail.com>
2022-10-12 15:13:03 +0900

    save

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,10 +10,11 @@ class Play extends Phaser.Scene {
         const map = this.createMap();
         const layers = this.createLayers(map);
 
-        const player = this.createPlayer();
+        this.player = this.createPlayer();
 
-        this.physics.add.collider(player, layers.platfromsColliders);
-
+        this.playerSpeed = 200;
+        this.physics.add.collider(this.player, layers.platfromsColliders);
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     createMap() {
@@ -44,6 +45,18 @@ class Play extends Phaser.Scene {
         player.body.setGravityY(500);
         player.setCollideWorldBounds(true);
         return player;
+    }
+
+    update() {
+        const { left, right } = this.cursors;
+
+        if(left.isDown) {
+            this.player.setVelocityX(-this.playerSpeed);
+        }else if(right.isDown) {
+            this.player.setVelocityX(this.playerSpeed);
+        }else{
+            this.player.setVelocityX(0);
+        }
     }
 }
 
